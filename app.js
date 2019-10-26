c33b91a883b96be2bc8fdd901a61fdf70193acd5
Michael J Peppers <mjpeppersdev@protonmail.com>
2019-10-26 14:11:45 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,38 @@
+let level = 0;
+
+function preload(){
+  sound = loadSound('https://res.cloudinary.com/homesbysadeth/video/upload/v1572124096/static/SANTANA_-_Smooth_1_c4xs4d.mp3');
+}
+function setup() {
+  cnv = createCanvas(window.innerWidth,window.innerHeight);
+  amplitude = new p5.Amplitude();
+  cnv.mouseClicked(function() {
+    $('body').toggleClass('playing');
+    if (sound.isPlaying() ){
+      sound.stop();
+    } else {
+      sound.play();
+    }
+  });
+}
+
+
+function draw() {
+  TweenMax.to($('.blade'), 0.25, {
+        css: { 
+          transform: 'scale('+(2 + (level * 20))+') rotate('+(level * (-400))+'deg)'
+        }, 
+        ease:Expo.easeOut, 
+        overwrite: 'all' 
+    
+    });
+  background(0);
+  fill(255);
+  level = amplitude.getLevel();
+  if (level > 0.2){
+    $('#wrap').addClass('spin');
+    setTimeout(function(){
+      $('#wrap').removeClass('spin');
+    }, 1000);
+  }
+}
